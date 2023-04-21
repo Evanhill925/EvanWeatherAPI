179b4e52d622a45072d93daa9ef4c22abe92fa8e
Evan <Evanhill925@gmail.com>
2023-04-20 17:16:08 -0700

    Added CSS styles and updated button duplication issue

--- main.js
+++ main.js
@@ -1,16 +1,55 @@
-document.querySelector('button').addEventListener('click',getLocation)
+document.getElementById('buttonMain').addEventListener('click',getLocation)
 
-function getLocation(){
-    console.log('click')
-  let weatherLocation =  document.querySelector('input').value 
-    fetch(`https://api.weatherapi.com/v1/current.json?key=b60121d8d80c49b5918195837231504&q=${weatherLocation}`)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            document.querySelector('h2').innerText = data.location.name + "  " + data.location.region
-            document.querySelector('h3').innerText = data.current.condition.text
-            document.getElementById('temps').innerText = data.current.temp_c + "°C"
 
+
+async function getLocation(){
+  let weatherLocation =  document.getElementById('input').value 
+  let response = await fetch(`https://api.weatherapi.com/v1/current.json?key=b60121d8d80c49b5918195837231504&q=${weatherLocation}`)
+   data = await response.json();
+           
+            document.getElementById('date').innerText = data.current.last_updated.slice(0,10);
+            document.getElementById('location').innerText = data.location.name;
+            document.getElementById('region').innerText = data.location.region;
+            document.getElementById('cond').innerText = data.current.condition.text;
+            document.getElementById('temps').innerText = data.current.temp_c + "°C";
+            
+
+
+            let tempC = document.createElement('button')
+            let tempF = document.createElement('button')
+
+
+           tempC.setAttribute('id','tempC')
+           tempF.setAttribute('id','tempF') 
+        //    document.getElementById('weatherCard').appendChild(tempC)
+        //    document.getElementById('weatherCard').appendChild(tempF)
+           
+           if(document.getElementById('tempF') !== null) {
+            console.log('Buttons are there')
+           } else {
+            console.log('ran else statement')
+            document.getElementById('weatherCard').appendChild(tempF)
+            
+         }
+           if(document.getElementById('tempC') !== null) {
+            console.log('Buttons are there')
+            
+           } else {
+            console.log('ran else statement')
+            document.getElementById('weatherCard').appendChild(tempC)
+            
+          }
+          document.getElementById('tempF').innerText = "°F"
+          document.getElementById('tempC').innerText = "°C"
+
+        
+ 
+           
+            
+        document.getElementById('tempF').addEventListener('click',getF)
+
+        document.getElementById('tempC').addEventListener('click',getC)
+            
             if (data.current.condition.code === 1000){
                 document.getElementById('weatherCard').style.backgroundImage = "url('images/sun.png')"
             } else if([1063,1150,1153,1168,1171,1180,1183,1186,1189,1192,1195,1198,1201,1240,1243,1246].includes(data.current.condition.code)) {
@@ -26,11 +65,24 @@ function getLocation(){
             } else if([1087,1273,1276].includes(data.current.condition.code)){
                 document.getElementById('weatherCard').style.backgroundImage = "url('images/storm.png')"
             } 
-        })
-        .catch(err =>{
-            console.log(`error ${err}`)
-        })
-}
+            return data
+            
+        }
+
+        function getF(){
+            document.getElementById('temps').innerText = data.current.temp_f + "°F"
+        }
+
+        function getC(){
+            document.getElementById('temps').innerText = data.current.temp_c + "°C"
+        }
+        // .catch(err =>{
+        //     console.log(`error ${err}`)
+        // })
+
+
+
+        
 
 
 
